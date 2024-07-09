f065a040ac4d0da59fa3de91b691c524a201b2e4
Lale Mammadli <yavuzadpu@gmail.com>
2024-07-09 23:34:10 +0400

    added borrowers

--- src/main/java/com/informix/bookstore/controller/AdminController.java
+++ src/main/java/com/informix/bookstore/controller/AdminController.java
@@ -5,8 +5,8 @@ import com.informix.bookstore.repository.BorrowerProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +30,13 @@ public class AdminController {
         List<BorrowerProfile> borrowerProfiles = borrowerProfileRepository.findAll();
         model.addAttribute("borrowers", borrowerProfiles);
         return "borrowers";
+    }
+    @GetMapping("/borrowers/delete/{id}")
+    public String deleteBorrower(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
+
+            borrowerProfileRepository.deleteById(id);
+
+
+        return "redirect:/borrowers";
     }
 }
